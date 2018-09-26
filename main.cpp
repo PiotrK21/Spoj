f16f1c9071730a22bec96f55eacd5201db20e8b5
Piotrek <piotrek0309@vp.pl>
2018-09-26 21:55:48 +0200

    'Zmiany_26.09.18'

--- main.cpp
+++ main.cpp
@@ -5,13 +5,6 @@
 
 using namespace std;
 
-SMALL_RECT sr; // struktura wykorzystywana do ustawienia rozmiaru okna
-sr.Left = 0; // na zero
-sr.Top = 0; // na zero
-sr.Right = c2.X-1; // szerokość o 1 mniejsza od bufora
-sr.Bottom = c2.Y-1; // wysokość o 1 mniejsza od bufora
-SetConsoleWindowInfo(handle,true,&sr); // ustawia rozmiar okna (jednostka to szerokość i wysokość pojedynczego znaku)
-
 int main()
 {
     int A=1, wybor;
---
+++ zadania.h
@@ -0,0 +1,13 @@
+#ifndef zadania_h
+#define zadania_h
+
+void Menu();
+void Fun_wybor(int);
+void Problem_Collatza();
+void Czy_umiesz_potegowac();
+void Dwie_cyfry_silni();
+void Liczby_pierwsze();
+void Dwumiany();
+void Imieniny();
+
+#endif
